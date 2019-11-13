d0bca16cd7addd13f557b62075130128922f15df
Toni-Ty <antonique2479@yahoo.com>
2019-11-13 08:03:15 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 def say_hello(name = "Ruby Programmer")
   puts "Hello #{name}!"
+=======
+say_hello(name = "Hello Ruby Programmer!")
+  puts "Hello , #{name}"
+>>>>>>> 8b8412db12b726babf5c7c2a5b4c97249a2f4cad
 end
